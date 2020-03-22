349409aff06a2194fefceb570d47ed6905984e4d
Paulo Silva <mrpvsilva@gmail.com>
2020-03-22 13:31:59 -0300

    Updates

---
+++ precache-manifest.08a8c32cc56dd921eb006dad8edbcee5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9430742b0a44bbbf46da79081e35e00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77ec05a6c189b0afede6",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "59360ac6827bb4a0ba8f",
+    "url": "/static/css/main.a4e28c59.chunk.css"
+  },
+  {
+    "revision": "77ec05a6c189b0afede6",
+    "url": "/static/js/2.ac7b83ad.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ac7b83ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59360ac6827bb4a0ba8f",
+    "url": "/static/js/main.4a2fbbca.chunk.js"
+  },
+  {
+    "revision": "dfece7d17314e93d16d2",
+    "url": "/static/js/runtime-main.75098e12.js"
+  }
+]);
