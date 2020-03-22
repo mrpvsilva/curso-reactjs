cb462f9eaa7bc93c242dc391cb940abeca8f871f
Paulo Silva <mrpvsilva@gmail.com>
2020-03-22 17:29:48 -0300

    Updates

---
+++ precache-manifest.fccb7b5d6215319e71748f9067a78b32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c8ff694f2d3a29f149c46d4e48029f3",
+    "url": "/curso-reactjs/index.html"
+  },
+  {
+    "revision": "77ec05a6c189b0afede6",
+    "url": "/curso-reactjs/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "e87986e510ebfb602923",
+    "url": "/curso-reactjs/static/css/main.a4e28c59.chunk.css"
+  },
+  {
+    "revision": "77ec05a6c189b0afede6",
+    "url": "/curso-reactjs/static/js/2.ac7b83ad.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/curso-reactjs/static/js/2.ac7b83ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e87986e510ebfb602923",
+    "url": "/curso-reactjs/static/js/main.b34bf839.chunk.js"
+  },
+  {
+    "revision": "0a00236aecb8a4a01199",
+    "url": "/curso-reactjs/static/js/runtime-main.cbc0d90b.js"
+  }
+]);
