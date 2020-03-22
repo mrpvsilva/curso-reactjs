823faa40fe3b1d93c547c1548d77cecdca03cd6d
Paulo Silva <mrpvsilva@gmail.com>
2020-03-22 11:52:42 -0300

    request http com axios

--- src/Http.js
+++ src/Http.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 import axios from 'axios';
 
 class Http extends Component {
@@ -11,6 +10,8 @@ class Http extends Component {
             persons: []
         }
     }
+
+    
 
     componentDidMount() {
         axios
