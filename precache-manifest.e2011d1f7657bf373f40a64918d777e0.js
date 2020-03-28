d83b537aae7feb180a0bd2ebaa7287c353596966
Paulo Victor Luciano Da Silva <paulo.silva@venturus.org.br>
2020-03-27 21:14:07 -0300

    Updates

---
+++ precache-manifest.e2011d1f7657bf373f40a64918d777e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e900c8645903f7b06c9c4d54a843092",
+    "url": "/curso-reactjs/index.html"
+  },
+  {
+    "revision": "a764e248d277be29ec36",
+    "url": "/curso-reactjs/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "586324802f5f7a8915f1",
+    "url": "/curso-reactjs/static/css/main.d615c08d.chunk.css"
+  },
+  {
+    "revision": "a764e248d277be29ec36",
+    "url": "/curso-reactjs/static/js/2.b0590377.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/curso-reactjs/static/js/2.b0590377.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "586324802f5f7a8915f1",
+    "url": "/curso-reactjs/static/js/main.97cb9bdb.chunk.js"
+  },
+  {
+    "revision": "0a00236aecb8a4a01199",
+    "url": "/curso-reactjs/static/js/runtime-main.cbc0d90b.js"
+  }
+]);
